eeb2d61ea532cdd77e8f70652224a6e311067320
matthatters <44166302+matthatters@users.noreply.github.com>
2018-10-15 10:02:20 -0400

    Add file
    I hope this woks

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+#My FizzBuzz Program in Python
+#Which I spent all too much time thinking about
+
+for number in range(1, 34):
+    word = ''
+    if (number%3 == 0):
+        word+="Fizz"
+    if (number%5 == 0):
+        word+="Buzz"
+    print(word or number)
+    
+
